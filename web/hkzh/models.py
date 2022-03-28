58c40abaedda1f46ca0f0b6392e923ebb8de80c1
Rowan Deckow <RowanDeckow@outlook.com>
2022-03-28 16:59:05 +0800

    Update models.py

--- web/hkzh/models.py
+++ web/hkzh/models.py
@@ -110,7 +110,7 @@ class PayCard(models.Model):
     amount = models.FloatField(default=0)
 
     def __str__(self):
-        return self.card_info+'(￥'+self.amount+')'
+        return self.card_info+'($'+self.amount+')'
 
 class Payment(models.Model):
     card = models.ForeignKey(PayCard, on_delete=models.CASCADE)
